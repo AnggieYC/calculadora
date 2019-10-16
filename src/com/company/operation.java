b7d635ec4604e73747997cf980165bf64f04c648
anggieyc <anggie.yengle@utec.edu.pe>
2019-10-16 11:48:02 -0500

    commit

--- src/com/company/operation.java
+++ src/com/company/operation.java
@@ -1,4 +1,49 @@
 package com.company;
 
 public class operation {
+    public int precedence (char myChar){
+        if(myChar == '^') return 3;
+        else if(myChar == '*' || myChar == '/') return 2;
+        else if(myChar == '+' || myChar == '-') return 1;
+        else return 0;
+    };
+
+    boolean isOperand(char myChar){
+        if (myChar>='0' && myChar<='9') return true;
+        else if (myChar>='a' && myChar<='z') return true;
+        else if (myChar>='A' && myChar<='Z') return true;
+        return false;
+    }
+
+    boolean isVariable(char myChar){
+        if (myChar>='a' && myChar<='z') return true;
+        else if (myChar>='A' && myChar<='Z') return true;
+        return false;
+    }
+
+    boolean isOperator(char myChar) {
+        return myChar == '+' || myChar == '-' || myChar == '*' || myChar == '/' || myChar == '^';
+    }
+
+    boolean isParentesis(char myChar){
+        return myChar =='(' || myChar == ')';
+    }
+    boolean isLeftParentesis(char myChar){
+        return myChar == '(';
+    }
+    boolean isRightParentesis(char myChar){
+        return myChar == ')';
+    }
+    boolean isPlusOrMinus(char myChar){
+        return myChar == '-' || myChar == '+';
+    }
+    boolean isPlus(char myChar) {
+        return myChar == '+';
+    }
+    boolean isMinus(char myChar) {
+        return myChar == '-';
+    }
+    boolean isMult(char myChar){
+        return  myChar == '*';
+    }
 }
