40c569f2b0223bb2cebfe4ba7107f1d55495a5ca
L4mb0 <juan.navarro@utec.edu.pe>
2019-10-16 11:59:26 -0500

    operands

--- src/com/company/operation.java
+++ src/com/company/operation.java
@@ -1,4 +1,48 @@
 package com.company;
 
 public class operation {
+
+	public int precedence(char myChar){
+		if(myChar == '^') return 3;
+        	else if(myChar == '*' || myChar == '/') return 2;
+        	else if(myChar == '+' || myChar == '-') return 1;
+        	else return 0;
+	}
+
+
+	public boolean isOperator(char myChar) {
+		return myChar == '+' || myChar == '-' || myChar == '*' || myChar == '/' || myChar == '^';
+    	}
+
+    	public boolean isParentesis(char myChar){
+        	return myChar =='(' || myChar == ')';
+    	}
+    
+	public boolean isLeftParentesis(char myChar){
+        	return myChar == '(';
+    
+	}
+    
+	public boolean isRightParentesis(char myChar){
+        	return myChar == ')';
+    	}
+    
+	public boolean isPlusOrMinus(char myChar){
+        	return myChar == '-' || myChar == '+';
+    	}
+    
+	public boolean isPlus(char myChar) {
+        	return myChar == '+';
+    	}
+    
+	public boolean isMinus(char myChar) {
+        	return myChar == '-';
+    	}
+    
+	public boolean isMult(char myChar){
+        	return  myChar == '*';
+    	}
+
+
+
 }
