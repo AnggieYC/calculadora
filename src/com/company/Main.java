00462c47023915258604836ef80a4524b6f3f097
juan4056 <juan.vargas@utec.edu.pe>
2019-10-16 12:43:32 -0500

    e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        operation op = new operation("1+2");
-        op.operate();
+        operation op = new operation("1.0+2.0");
+        System.out.println(op.operate());
     }
 }
